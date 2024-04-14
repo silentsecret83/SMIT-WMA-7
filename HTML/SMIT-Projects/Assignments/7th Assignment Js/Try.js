a34446712a2436eff1a1e107499222696ab747d7
silentsecret83 <mu08823@gmail.com>
2024-04-15 03:22:01 +0500

    js

--- HTML/SMIT-Projects/Assignments/7th Assignment Js/Try.js
+++ HTML/SMIT-Projects/Assignments/7th Assignment Js/Try.js
@@ -211,10 +211,31 @@
 // Percentage >= 40 % : Grade E
 // Percentage < 40 % : Grade F
 
-//13:-  Write a js program to input the basic salary of an employee and calculate its Gross salary according to the following:
-//Basic Salary <= 10000 : HRA = 20%, DA = 80%
-//Basic Salary <= 20000 : HRA = 25%, DA = 90%
-//Basic Salary > 20000 : HRA = 30%, DA = 95%
+// 13:-  Write a js program to input the basic salary of an employee and calculate its Gross salary according to the following:
+// Basic Salary <= 10000 : HRA = 20%, DA = 80%
+// Basic Salary <= 20000 : HRA = 25%, DA = 90%
+// Basic Salary > 20000 : HRA = 30%, DA = 95%
+
+let basic_salary = parseFloat(
+  prompt("Enter the basic salary of the employee:")
+);
+var HRA;
+var DA;
+var gross_salary;
+
+if (basic_salary <= 10000) {
+  HRA = 0.2 * basic_salary;
+  DA = 0.8 * basic_salary;
+} else if (basic_salary <= 20000) {
+  HRA = 0.25 * basic_salary;
+  DA = 0.9 * basic_salary;
+} else {
+  HRA = 0.3 * basic_salary;
+  DA = 0.95 * basic_salary;
+}
+gross_salary = basic_salary + HRA + DA;
+console.log(`Gross salary: ${gross_salary}`);
+alert(`Gross salary: ${gross_salary}`);
 
 //14:-    Write a js program to input electricity unit charges and calculate total electricity bill according to the given condition:
 //For the first 50 units Rs. 0.50/unit
---
+++ HTML/SMIT-Projects/Assignments/8th Assignment/loop.js
@@ -0,0 +1,49 @@
+// 1. Write a JS code to print numbers from 1 to 10
+
+for (let index = 1; index <= 10; index++) {
+  console.log(index);
+}
+
+// 2. Write a JS code to print Even numbers in given array
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log("Even numbers in the array:");
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 == 0) {
+//             console.log(array[i]);
+//         }
+//     }
+
+// 3.  Write a JS code to delete all occurrence of element in given array
+
+// const array = [6, 2, 3, 4, 2, 6, 7, 2, 9, 10, 2];
+// const val = 2;
+
+// function removeAllOccurrences(arr, value) {
+//     return arr.filter(item => item !== value);
+// }
+
+// const newArray = removeAllOccurrences(array, val);
+// console.log("Array after removing all occurrences of", val + ":", newArray);
+
+// 4. Write a JS code to find the power of a number using for loop
+
+//// Function to calculate power of a number using for loop
+// function power(base, exponent) {
+//     let result = 1;
+//     for (let i = 0; i < exponent; i++) {
+//         result *= base;
+//     }
+//     return result;
+// }
+// const base = parseFloat(prompt("Enter the base:"));
+// const exponent = parseInt(prompt("Enter the exponent:"));
+
+// if (isNaN(base) || isNaN(exponent)) {
+//     console.log("Please enter valid numbers.");
+//     alert("Please enter valid numbers.");
+// } else {
+//     const result = power(base, exponent);
+//     console.log(base + " raised to the power of " + exponent + " is:", result);
+//     alert(base + " raised to the power of " + exponent + " is:" + result);
+// }
