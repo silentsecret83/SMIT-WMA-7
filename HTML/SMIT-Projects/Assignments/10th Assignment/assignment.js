d0f5ba9daedb8147edff712ae8e30454e4a0ae75
silentsecret83 <mu08823@gmail.com>
2024-05-15 16:42:41 +0500

    next

---
+++ HTML/SMIT-Projects/Assignments/10th Assignment/assignment.js
@@ -0,0 +1,18 @@
+// Task 1: Object Creation
+
+// Create an empty object and assign it to a variable.
+// Create an object with properties for name, age, and profession.
+
+//  Create an empty object
+let emptyObject = {};
+
+//  Create an object with properties for name, age, and profession
+let person = {
+  name: "Jane Doe",
+  age: 28,
+  profession: "Graphic Designer",
+};
+
+// Output the objects to verify
+console.log(emptyObject); // Output: {}
+console.log(person);
