d57b47eee800cbbfe3fae802d75ff6681cff41a9
silentsecret83 <mu08823@gmail.com>
2024-07-02 16:17:59 +0500

    check

--- HTML/SMIT-Projects/JS/try.js
+++ HTML/SMIT-Projects/JS/try.js
@@ -8,3 +8,54 @@
 
 // name = "Usama";
 // alert(name);
+// var lastName = "Smith";
+// var cityOfOrigin = "New Orleans";
+// var aussieGreeting = "g'Day";
+// var nickname = "Bub";
+// var thanx = "Thanks for your input!";
+// alert(thanx);
+
+// Ch 3 Variables for Numbers
+
+// var num = 23;
+// newnum = num + 7;
+// console.log(newnum);
+// alert(newnum);
+
+// var num1 = 45;
+// var num2 = 35;
+// total = num1 + num2;
+// alert(total);
+// console.log(total);
+
+// var num = 80;
+// num = num + 10;
+// console.log(num);
+// alert(num);
+// total = num + 30;
+// console.log(total);
+// alert(total);
+
+//  var 1stPresident = "Washington"; (we does not use the num in the start of the var)
+// var prezWhoCame1st = "Washington";
+// alert(144);
+
+// CH 4 Variable Names Legal and Illegal
+// camelCase
+// example:
+// userResponse
+// userResponseTime
+// userResponseTimeLimit
+
+//  Ch 5 : Math expressions: Familiar operators
+
+// var num1 = 4 + 5;
+// var num2 = 5 - 2;
+// var num3 = 6 * 3;
+// var num4 = 8 / 4;
+// var num5 = 100 % 60;
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(num4);
+// alert(num5);
